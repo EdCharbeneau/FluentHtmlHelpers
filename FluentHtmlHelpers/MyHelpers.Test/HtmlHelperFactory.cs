60f87aac53c928c6eaae4933def5278dbd8f7c73
Ed Charbeneau <ed.charbeneau@gmail.com>
2012-09-15 15:29:47 -0400

    Added an HTML helper factory for testing.
    System.Web
    System.Web.MVC
    Required for testing HTML Helpers

---
+++ FluentHtmlHelpers/MyHelpers.Test/HtmlHelperFactory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyHelpers.Test
+{
+    class HtmlHelperFactory
+    {
+        public static HtmlHelper Create()
+        {
+            var vc = new ViewContext();
+            vc.HttpContext = new FakeHttpContext();
+            var html = new HtmlHelper(vc, new FakeViewDataContainer());
+            return html;
+        }
+
+        private class FakeHttpContext : HttpContextBase
+        {
+            private readonly Dictionary<object, object> items = new Dictionary<object, object>();
+
+            public override IDictionary Items
+            {
+                get
+                {
+                    return items;
+                }
+            }
+        }
+
+        private class FakeViewDataContainer : IViewDataContainer
+        {
+            private ViewDataDictionary viewData = new ViewDataDictionary();
+            public ViewDataDictionary ViewData
+            {
+                get
+                {
+                    return viewData;
+                }
+                set
+                {
+                    viewData = value;
+                }
+            }
+        }
+    }
+}
---
+++ FluentHtmlHelpers/MyHelpers.Test/HtmlHelperTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyHelpers.Test
+{
+    [TestClass]
+    public class HtmlHelperTest
+    {
+        [TestMethod]
+        public void ShouldCreateHtmlHelper()
+        {
+            //arrange
+            var html = HtmlHelperFactory.Create();
+
+            //assert
+            Assert.IsNotNull(html);
+        }
+    }
+
+}
